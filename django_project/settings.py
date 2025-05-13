612233aaf5b965b9a06386ef9ac5afe547cd7641
Andrew Ahn <andrew.ahn@hotmail.co.uk>
2025-05-13 09:33:12 +0100

    updates, page 89 of Django book

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*bx3hk7m=hk+r5$hry4416&4r7eg^uvl*f6w&7rcml-hjc+1m&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
